302b6ba89743d79127ea3ad1bf99c0bf813b06ea
elen <elena.sergeeva.99@list.ru>
2023-02-13 18:14:06 +0300

    simple seo optimization

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,9 +2,17 @@ import {Head, Html, Main, NextScript} from 'next/document'
 
 export default function Document() {
     return (
-        <Html lang="en">
+        <Html lang="en" prefix="og: https://ogp.me/ns#">
             <Head>
                 <link rel="icon" href="/favicon.ico"/>
+                <meta property="og:title" content="Photographmimi"/>
+                <meta property="og:description"
+                      content="Фотограф Ростов-на-Дону"/>
+                <meta property="og:locale" content="ru_RU"/>
+                <meta property="og:site_name" content="Photographmini"/>
+                <meta property="og:url" content="https://sergeevaevi.github.io/photographmini_i/"/>
+                <meta property="og:image" content="https://sergeevaevi.github.io/photographmini_i/photo.jpg"/>
+                <meta property="og:type" content="website"/>
             </Head>
             <body>
             <Main/>
